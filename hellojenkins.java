fecc61587c4b66e01074fc15915203001857b243
premkumar <premkumarbsct@gmail.com>
2025-03-20 20:11:42 +0530

    adding 3 files

---
+++ hellojenkins.java
@@ -0,0 +1,8 @@
+public class hellojenkins {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Jenkins");
+
+	}
+
+}
---
+++ hellowipro.java
@@ -0,0 +1,8 @@
+public class hellowipro {
+
+	public static void main(String[] args) {
+	System.out.println("Hello wipro");
+
+	}
+
+}
